49ee4883927d29e215379a23ce689fed8f2f9537
A20021 <aldrich.iberri228878@potros.itson.edu.mx>
2022-10-24 08:56:14 -0700

    Avanze de proyecto

---
+++ Noviembre.Core/Entidades/Cita.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noviembre.Core.Entidades
+{
+    public class Cita{
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public Modulo Modulo { get; set; }
+        public Ciudadano Ciudadano { get; set; }
+    }
+}
