d4e7db213cf0db10df242f7a5e918a5ea5b96aa3
A20021 <aldrich.iberri228878@potros.itson.edu.mx>
2022-10-26 10:53:28 -0700

    Agregue clases faltantes.

--- Noviembre.Core/Entidades/Cita.cs
+++ Noviembre.Core/Entidades/Cita.cs
@@ -11,5 +11,8 @@ namespace Noviembre.Core.Entidades
         public DateTime Fecha { get; set; }
         public Modulo Modulo { get; set; }
         public Ciudadano Ciudadano { get; set; }
+        public Tramite Tramite { get; set; }
+        public DocumentoNacionalidad DocumentoNacionalidad { get; set; }
+        public ComprobanteDomicilio ComprobanteDomicilio { get; set; }
     }
 }
